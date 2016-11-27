f5ba60944c798a73d36da8c2e7a2451234559548
budougumi0617 <budougumi0617@gmail.com>
2016-11-28 01:05:14 +0900

    Add sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,25 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @file
+ * @par File Name:
+ * SampleTest.java
+ * @author budougumi0617
+ * @date Created on 2016/11/28
+ */
+
+/**
+ * @author budougumi0617
+ * @note no comment
+ */
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertThat(1, is(1));
+	}
+
+}
