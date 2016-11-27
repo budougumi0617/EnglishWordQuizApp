9a1375578357060864aa1b432dcf1ab57e305542
budougumi0617 <budougumi0617@gmail.com>
2016-11-27 20:05:54 +0900

    Add Build, and CI settings

---
+++ src/main/java/Sample.java
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @par File Name:
+ * Sample.java
+ * @author budougumi0617
+ * @date Created on 2016/11/27
+ */
+
+/**
+ * @author budougumi0617
+ * @note no comment
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Sample Main.");
+
+	}
+
+}
