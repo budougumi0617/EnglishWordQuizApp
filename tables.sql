3597d3c28a7f68744964a29f9e89fddb25edbbd6
ema195y <yuka.yoshikawa@jrits.ricoh.co.jp>
2016-12-12 17:47:55 +0900

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -3,6 +3,6 @@ id int primary key not null auto_increment,
 word char(255) not null,
 part enum('名詞','代名詞','動詞','形容詞','副詞','冠詞','助動詞','前置詞','疑問詞','接続詞') not null,
 mean char(255) not null,
-created_at timestamp not null,
-updated_at timestamp not null default current_timestamp on update current_timestamp
+created_at timestamp not null default 0,
+updated_at timestamp not null default 0 on update current_timestamp
 );
