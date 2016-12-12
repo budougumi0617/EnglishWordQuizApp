df1cd7635df36ba9146ba78ee5b28cd146dc3e38
budougumi0617 <budougumi0617@gmail.com>
2016-12-12 21:01:51 +0900

    Update SQL

--- tables.sql
+++ tables.sql
@@ -3,6 +3,6 @@ id int primary key not null auto_increment,
 word char(255) not null,
 part enum('名詞','代名詞','動詞','形容詞','副詞','冠詞','助動詞','前置詞','疑問詞','接続詞') not null,
 mean char(255) not null,
-created_at timestamp not null default 0,
-updated_at timestamp not null default 0 on update current_timestamp
-);
+created_at timestamp not null default current_timestamp,
+updated_at timestamp not null default current_timestamp on update current_timestamp
+) default charset=utf8; /* Need to define charset expressly if you want to use Japanese in SQL statements on Linux. */
