6369736549b2d15677670ac18ad68495529a4982
Yuka Yoshikawa <yuka.yoshikawa@jrits.ricoh.co.jp>
2016-12-12 16:11:31 +0900

    Midufy  travis MySQL setting

--- src/main/java/quiz/model/Part.java
+++ src/main/java/quiz/model/Part.java
@@ -13,4 +13,21 @@ public enum Part {
 
 	名詞, 代名詞, 動詞, 形容詞, 副詞, 冠詞, 助動詞, 前置詞, 疑問詞, 接続詞,
 
+	// noun("名詞"), pronoun("代名詞"), verb("動詞"), adjective("形容詞"), adverb("副詞"),
+	// article("冠詞"), modalverb(
+	// "助動詞"), preposition("前置詞"), interrogatives("疑問詞"), conjunction("接続詞"),;
+	//
+	/// ** メンバ変数の定義 */
+	// private String name;
+	//
+	/// ** コンストラクタの実装 */
+	// private Part(String name) {
+	// this.name = name;
+	// }
+	//
+	/// ** メソッドのオーバーライド */
+	// public String toString() {
+	// return name;
+	// }
+
 }
