b9a98ba7fa9241fca07ea1e401f976f8893108b6
Yuka Yoshikawa <yuka.yoshikawa@jrits.ricoh.co.jp>
2016-12-14 11:12:13 +0900

    Create Error Dialog class

---
+++ src/main/java/quiz/view/ErrorDialog.java
@@ -0,0 +1,28 @@
+/**
+ * @file 2016/12/13
+ */
+package quiz.view;
+
+import javax.swing.JOptionPane;
+
+/**
+ * エラーダイアログを生成する
+ *
+ * @author Yuka Yoshikawa
+ *
+ */
+public class ErrorDialog extends JOptionPane {
+
+	/**
+	 * エラーダイアログを生成するメソッド
+	 *
+	 * @param errorMessage
+	 *            String型 エラー内容のメッセージ
+	 */
+
+	static public void showErrorDialog(String errorMessage) {
+		Object[] selectvalue = { "閉じる" };
+		showOptionDialog(null, errorMessage, "エラー発生", YES_NO_OPTION, ERROR_MESSAGE, null, selectvalue, null);
+	}
+
+}
